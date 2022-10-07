10286f42ad4f837d6d960b8fbf2d4ade70bb14aa
DESKTOP-PO3GE90\Argy <argysynanis@gmail.com>
2022-10-07 11:52:11 +0100

    first commit

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 function SearchBox() {
+  const [search, setSearch] = useState("");
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className="mt-5 relative">
-      <input placeholder="Enter a search term" className="px-5 py-3 border-gray-400 border rounded w-full" />
+      <input type="text" placeholder="Enter a search term" className="px-5 py-3 border-gray-400 border rounded w-full" value={search} onChange={searchHandler}/>
 
       <FaSearch className="absolute top-3.5 right-3.5 text-gray-400" size={20} />
     </div>
