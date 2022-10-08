3de041fa6819277cbd10b27a8a04b361154e4204
DESKTOP-PO3GE90\Argy <argysynanis@gmail.com>
2022-10-08 12:38:31 +0100

    adding context for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Header from './Header';
 import PropertyCard from './PropertyCard';
+import SearchContext from './context/search/SearchContext';
 
 function App() {
-  const [properties, setProperties] = useState();
-
   // use this state to keep track of the user's saved/bookmarked properties
   const [savedProperties, setSavedProperties] = useState([]);
+  const [properties, setProperties] = useState();
+  const { searchTerm, setSearchTerm } = useContext(SearchContext);
 
   useEffect(() => {
     const fetchPropertyData = async () => {
@@ -21,10 +22,21 @@ function App() {
 
   return (
     <div className="container mx-auto my-5">
-      <Header />
+      <Header onSearch={setSearchTerm} />
 
       <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {!!properties && properties.map((property) => <PropertyCard key={property.property_id} property={property} />)}
+        {!!properties &&
+          properties
+            .filter((property) => {
+              return searchTerm === undefined || searchTerm === ''
+                ? property
+                : property.short_description
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase());
+            })
+            .map((property) => (
+              <PropertyCard key={property.property_id} property={property} />
+            ))}
       </div>
     </div>
   );
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 
-function SearchBox() {
-  const [search, setSearch] = useState("");
-
+function SearchBox({ setSearch }) {
   const searchHandler = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   return (
     <div className="mt-5 relative">
-      <input type="text" placeholder="Enter a search term" className="px-5 py-3 border-gray-400 border rounded w-full" value={search} onChange={searchHandler}/>
+      <input
+        type="text"
+        placeholder="Enter a search term"
+        className="px-5 py-3 border-gray-400 border rounded w-full"
+        onChange={searchHandler}
+      />
 
-      <FaSearch className="absolute top-3.5 right-3.5 text-gray-400" size={20} />
+      <FaSearch
+        className="absolute top-3.5 right-3.5 text-gray-400"
+        size={20}
+      />
     </div>
   );
-};
+}
 
 export default SearchBox;
